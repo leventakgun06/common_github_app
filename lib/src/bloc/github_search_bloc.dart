7ac1c49021560653e4128916e5193e1bb086b980
Levent Akgün <leventakgun@apple-MacBook-Pro.local>
2023-09-10 18:14:57 +0300

    Package Update

--- lib/src/bloc/github_search_bloc.dart
+++ lib/src/bloc/github_search_bloc.dart
@@ -1,15 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:common_github_app/common_github_app.dart';
 import 'package:equatable/equatable.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 part 'github_search_event.dart';
+
+const _duration = Duration(milliseconds: 500);
+EventTransformer<Event> debounceEffect<Event>(Duration duration) {
+  return (events, mapper) => events.debounce(duration).switchMap(mapper);
+}
 
 class GithubSearchBloc extends Bloc<GithubSearchEvent, GithubSearchState> {
   GithubSearchBloc({required this.repository})
       : super(
           GithubSearchState.epmty(),
         ) {
-    on<TextChangedEvent>(_onTextChanged);
+    on<TextChangedEvent>(_onTextChanged,
+        transformer: debounceEffect(_duration));
   }
   final GithubRepository repository;
 
